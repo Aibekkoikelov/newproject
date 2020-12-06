25ee2a211e85038cfca796462fd1a40172cefbba
Aibek Koikelov <aibekkoikelov@gmail.com>
2020-12-06 23:53:20 +0600

    add new class change method in class Cat

---
+++ Cat/src/kg/newproject/aplication/Car.java
@@ -0,0 +1,14 @@
+package kg.newproject.aplication;
+
+public class Car extends NewProject{
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void result(int a, int b) {
+
+    }
+}
---
+++ Cat/src/kg/newproject/aplication/NewProject.java
@@ -0,0 +1,8 @@
+package kg.newproject.aplication;
+
+public abstract class NewProject {
+    public abstract void print ();
+
+    public abstract void result(int a, int b);
+
+}
